1836f1503d4ff7eb230bf64a875ff7a2a4d284ce
kavankumar <150102479+KavanBrahmbhatt0910@users.noreply.github.com>
2024-12-02 11:25:32 -0400

    feat(chat-bot): chat bot integrated with the frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Toaster } from "@/components/ui/toaster";
 import { UserProvider } from "./contexts/user-context";
+import DialogflowMessenger from "@/components/DialogflowMessenger";
 
 const default_font = Roboto({
   weight: ["300", "500", "700"],
@@ -44,6 +45,7 @@ export default function RootLayout({
           }
         >
           <Navbar />
+          <DialogflowMessenger />
           <main className="flex-grow flex justify-center items-center">
             {children}
           </main>
