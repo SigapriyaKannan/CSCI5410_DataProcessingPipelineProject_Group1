da20b9a8c8efe9bc1beb928b8f9c452790cafbb3
kavankumar <150102479+KavanBrahmbhatt0910@users.noreply.github.com>
2024-12-02 17:10:59 -0400

    fix(frontend): updated the url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const auth_api: string =
-  process.env.NEXT_PUBLIC_AUTH_API ?? "please-set-auth-api-env-variable";
+  process.env.NEXT_PUBLIC_AUTH_API ?? "https://jzi33rv5fg.execute-api.us-east-1.amazonaws.com/Dev";
